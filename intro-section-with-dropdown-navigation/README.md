8115e7442136ec93d87c3acf122796805fa4d8d8
Piyath Perera <piyathpereraa@gmail.com>
2022-05-16 16:08:57 +1000

    fix expand state logic in sub nav

--- intro-section-with-dropdown-navigation/README.md
+++ intro-section-with-dropdown-navigation/README.md
@@ -91,3 +91,9 @@ We love receiving feedback! We're always looking to improve our challenges and o
 This challenge is completely free. Please share it with anyone who will find it useful for practice.
 
 **Have fun building!** 🚀
+
+
+
+DO NOT REMOVE
+
+https://adrianroselli.com/2019/06/link-disclosure-widget-navigation.html
--- intro-section-with-dropdown-navigation/src/js/menu.js
+++ intro-section-with-dropdown-navigation/src/js/menu.js
@@ -1,19 +1,93 @@
-const toggle = document.querySelector("#toggle");
-const menu = document.querySelector("#menu");
+const toggle = document.getElementById("toggle");
+const menu = document.getElementById("menu");
 
 toggle.addEventListener("click", () => {
-
     const isVisible = menu.getAttribute("data-visible");
 
     if (isVisible === "false") {
         menu.setAttribute("data-visible", true);
-        toggle.setAttribute("aria-expanded",true);
+        toggle.setAttribute("aria-expanded", true);
     } else {
         menu.setAttribute("data-visible", false);
         toggle.setAttribute("aria-expanded", false);
     }
-
 });
 
-console.log("this works externally?");
-console.log("this works externally copied and changed?");
+
+// Get the nav by id
+var pNav = document.getElementById("Nav");
+
+function toggleSubNav(eID) {
+  try {
+    // Get all the top-level items and get the buttons
+    var allButtons = document.querySelectorAll("#Nav > ul > li > a + button");
+    // Get all sub-menus
+    var allSubMenus = document.querySelectorAll("#Nav > ul > li > a + button + ul");
+    if (eID !== "") {
+      // Get the button
+      var toggle = document.querySelector("#" + eID);
+      // Find out what the button controls
+      var subMenuIdent = toggle.getAttribute("aria-controls");
+      // Get that thing it controls
+      var subMenu = document.getElementById(subMenuIdent);
+      // Toggle text and expanded state
+      if (toggle.getAttribute("aria-expanded") === "false") {
+        // Loop through all buttons and mark as closed
+        for (var i = 0; i < allButtons.length; i++) {
+          allButtons[i].setAttribute("aria-expanded", "false");
+          // allButtons[i].setAttribute("aria-label", "Show");
+        //   allButtons[i].innerHTML = arrowShow;
+        }
+        // Now mark the chosen button as opened
+        toggle.setAttribute("aria-expanded", "true");
+        // toggle.setAttribute("aria-label", "Hide");
+      } else {
+        // Close chosen button; no need to loop through rest of nodes
+        toggle.setAttribute("aria-expanded", "false");
+        // toggle.setAttribute("aria-label", "Show");
+      }
+      // Expand or collapse
+      if (subMenu.style.display === "none") {
+        // Hide all submenus
+        for (var i = 0; i < allSubMenus.length; i++) {
+          allSubMenus[i].style.display = "none";
+        }
+        // Show chosen submenu
+        subMenu.style.display = "block";
+      } else {
+        // Close chosen menu; no need to loop through rest of nodes
+        subMenu.style.display = "none";
+      }
+    }
+    if (eID !== "") {
+    } else {
+      // Hide all submenus
+      for (var i = 0; i < allSubMenus.length; i++) {
+        allSubMenus[i].style.display = "none";
+      }
+      // Loop through all buttons and mark as closed
+      for (var i = 0; i < allButtons.length; i++) {
+        allButtons[i].setAttribute("aria-expanded", "false");
+        // allButtons[i].setAttribute("aria-label", "Show");
+        // allButtons[i].innerHTML = arrowShow;
+      }
+    }
+  } catch (e) {
+    console.log("toggleSubNav(): " + e);
+  }
+}
+
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) {
+    isEscape = evt.key == "Escape" || evt.key == "Esc";
+  } else {
+    isEscape = evt.keyCode == 27;
+  }
+  if (isEscape) {
+    //alert("Escape");
+    toggleSubNav("");
+  }
+};
+    
