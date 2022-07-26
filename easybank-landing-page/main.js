efd3d7ddf63ccf9990fc22be488789a52cb7e2d8
Piyath Perera <piyathpereraa@gmail.com>
2022-07-26 22:57:17 +1000

    fix functionality + styling except THE BG

--- easybank-landing-page/main.js
+++ easybank-landing-page/main.js
@@ -1,1 +1,21 @@
+//Toggle Navigation button
+const navButton = document.querySelector("button[aria-expanded]");
+const ariaHiddens = document.querySelectorAll("[aria-hidden]");
 
+
+function toggleNavigation({ target }) {
+    // const expanded = navButton.getAttribute("aria-expanded") === "true";
+    // navButton.setAttribute("aria-expanded", !expanded);
+
+    navButton.setAttribute("aria-expanded", target.getAttribute("aria-expanded") === "true" ? "false" : "true");
+    console.log("expanded", navButton.getAttribute("aria-expanded"));
+
+    ariaHiddens.forEach((ariaHidden) => {
+        ariaHidden.setAttribute("aria-hidden", ariaHidden.getAttribute("aria-hidden") === "true" ? "false" : "true");
+
+        console.log("hidden", ariaHidden.getAttribute("aria-hidden"));
+    });
+    
+}
+
+navButton.addEventListener("click", toggleNavigation);
