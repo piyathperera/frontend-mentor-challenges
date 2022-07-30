2942bd53b1615aa30f725eaae3b9924029f862df
Piyath Perera <piyathpereraa@gmail.com>
2022-07-30 22:10:34 +1000

    fix a11y issue of hiding focusable elements in nav

--- easybank-landing-page/main.js
+++ easybank-landing-page/main.js
@@ -1,22 +1,20 @@
 //Toggle Navigation button
 const navButton = document.querySelector("button[aria-expanded]");
-const navList = document.querySelector("ul[aria-hidden]");
+const navList = document.querySelector(".main-nav-list");
 const ariaHiddens = document.querySelectorAll("[aria-hidden]");
 
-
 function toggleNavigation({ target }) {
-    // const expanded = navButton.getAttribute("aria-expanded") === "true";
-    // navButton.setAttribute("aria-expanded", !expanded);
+    const expanded = navButton.getAttribute("aria-expanded") === "true";
+    navButton.setAttribute("aria-expanded", !expanded);
 
-    navButton.setAttribute("aria-expanded", target.getAttribute("aria-expanded") === "true" ? "false" : "true");
-    console.log("expanded", navButton.getAttribute("aria-expanded"));
+    const isVisible = navList.getAttribute("data-visible") === "true";
+    navList.setAttribute("data-visible", !isVisible);
 
     ariaHiddens.forEach((ariaHidden) => {
-        ariaHidden.setAttribute("aria-hidden", ariaHidden.getAttribute("aria-hidden") === "true" ? "false" : "true");
+        ariaHidden.setAttribute("aria-hidden", ariaHidden.getAttribute("aria-hidden") === "false" ? "true" : "false");
 
         console.log("hidden", ariaHidden.getAttribute("aria-hidden"));
     });
-    
 }
 
 navButton.addEventListener("click", toggleNavigation);
