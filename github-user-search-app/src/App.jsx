f1e90fa228f20839438caa81fbe0f9febea62e8d
Piyath Perera <piyathpereraa@gmail.com>
2022-10-23 12:25:51 +1100

    late commit - check later

--- github-user-search-app/src/App.jsx
+++ github-user-search-app/src/App.jsx
@@ -1,79 +1,97 @@
+import profile_img from "./assets/profile.svg";
+import icon_search from "./assets/icon-search.svg";
+
+function Search() {
+    return (
+        <article className="search">
+            <img src={icon_search} alt="" />
+            <input
+                type="search"
+                name="username"
+                id="search_username"
+                placeholder="Search Github username.."
+            />
+            {/* <span className="error">No results</span> */}
+            <button>Search</button>
+        </article>
+    );
+}
+
+function ProfileCard() {
+    return (
+        <article className="profile | region">
+            <header>
+                <img src={profile_img} alt="" />
+                <h2>The Octocat</h2>
+                <a href="">@octocat</a>
+                <span>Joined 25 Jan 2011</span>
+            </header>
+
+            <div className="flow">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Donec odio. Quisque volutpat mattis eros.
+                </p>
+                <div className="stats | grid">
+                    <div>
+                        <p>Repos</p>
+                        <span>8</span>
+                    </div>
+                    <div>
+                        <p>Followers</p>
+                        <span>3938</span>
+                    </div>
+                    <div>
+                        <p>Following</p>
+                        <span>9</span>
+                    </div>
+                </div>
+
+                <ul className="links | grid">
+                    <li>
+                        <a href="#location">
+                            <img src="" alt="" />
+                            San Francisco
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#social">
+                            <img src="" alt="" />
+                            Not Available
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#website">
+                            <img src="" alt="" />
+                            https://github.blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#work">
+                            <img src="" alt="" />
+                            @github
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </article>
+    );
+}
 
 function App() {
     return (
         <div className="App">
-            <main className="flow">
-                <header className="header | region">
-                    <h1>devfinder</h1>
-                    <button>Light/Dark</button>
-                </header>
-                <article className="flow">
-                    <input
-                        type="search"
-                        name="username"
-                        id="search_username"
-                        placeholder="Search Github username.."
-                    />
-                    <button>Search</button>
-                </article>
-                <article className="profile | region">
-                    <header>
-                        <img src="./assets/" alt="" />
-                        <h2>The Octocat</h2>
-                        <a href="">@octocat</a>
-                        <span>Joined 25 Jan 2011</span>
+            <div className="center">
+                <main className="flow wrapper">
+                    <header className="header | region">
+                        <h1>devfinder</h1>
+                        <button>Light/Dark</button>
                     </header>
-
-                    <div className="flow">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing
-                            elit. Donec odio. Quisque volutpat mattis eros.
-                        </p>
-                        <div className="stats | grid">
-                            <div>
-                                <p>Repos</p>
-                                <span>8</span>
-                            </div>
-                            <div>
-                                <p>Followers</p>
-                                <span>3938</span>
-                            </div>
-                            <div>
-                                <p>Following</p>
-                                <span>9</span>
-                            </div>
-                        </div>
-
-                        <ul className="links | grid">
-                            <li>
-                                <a href="#location">
-                                    <img src="" alt="" />
-                                    San Francisco
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#social">
-                                    <img src="" alt="" />
-                                    Not Available
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#website">
-                                    <img src="" alt="" />
-                                    https://github.blog
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#work">
-                                    <img src="" alt="" />
-                                    @github
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </article>
-            </main>
-            <footer></footer>
+                    <Search />
+                    <ProfileCard />
+                </main>
+                <footer></footer>
+            </div>
         </div>
     );
 }
